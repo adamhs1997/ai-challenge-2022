132d81035587bf350604cf6e647190db2d78668c
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2022-04-13 22:06:36 -0400

    Add 10-fold validation

--- ahs/pca_classifier.py
+++ ahs/pca_classifier.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+
+print("Start", datetime.now())
 
 with open("trainData.csv", 'r') as f:
     train = np.array(list(map(lambda s: list(map(int, s.strip().split(","))), f.readlines())))
@@ -15,7 +19,7 @@ raw_train_data, raw_test_data, train_labels, test_labels = train_test_split(trai
                                                                             test_size=0.33,
                                                                             random_state=42)
 
-print("Data split!")
+print("Data split!", datetime.now())
 
 
 def classify_with_pca(classifier, n_components):
@@ -35,8 +39,19 @@ def classify_with_pca(classifier, n_components):
     return preds
 
 
-predictions = classify_with_pca(RandomForestClassifier(), 50)
-with open("predictions.csv", 'w') as outf:
-    for p in predictions:
-        print(p, file=outf)
+def cross_validate(classifier, n_components):
+    pca = PCA(n_components=n_components)
+    train_data_transformed = pca.fit_transform(raw_train_data, train_labels)
 
+    cv = KFold(n_splits=10, random_state=0, shuffle=True)
+    scores = cross_val_score(classifier, train_data_transformed, train_labels, scoring='accuracy', cv=cv, n_jobs=3)
+    print('{} - cross-val done!'.format(datetime.now()))
+    print('Accuracy: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+
+
+# predictions = classify_with_pca(RandomForestClassifier(), 50)
+# with open("predictions.csv", 'w') as outf:
+#     for p in predictions:
+#         print(p, file=outf)
+
+cross_validate(RandomForestClassifier(), 50)
